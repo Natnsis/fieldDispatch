f39c726a1934865d6a215544048706cf271b3f63
natnael <nsisay49@gmail.com>
2025-10-05 18:01:56 +0300

    font and images

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,22 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import '@/global.css';
+import { Inter_400Regular } from "@expo-google-fonts/inter";
+
+SplashScreen.preventAutoHideAsync();
+
+const initialFonts = {
+  inter: Inter_400Regular,
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, fontError] = useFonts(initialFonts);
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, View, ImageBackground } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="bg-background-light h-screen">
+      <View className="relative w-full">
+        <ImageBackground
+          source={require("../assets/image/home.png")}
+          className="w-full h-[40%]"
+          resizeMode="cover"
+        >
+          <View>
+            <Text className="text-white">Dispatcher App</Text>
+            <Text>connect</Text>
+          </View>
+        </ImageBackground>
+      </View>
+      <View className="">
+        <Text className="font-roboto text-2xl text-center">Track, assign,</Text>
+        <Text>and manage deliveries</Text>
+        <Text>in real-time.</Text>
+      </View>
+    </SafeAreaView>
   );
 }
